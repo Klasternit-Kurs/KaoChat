73ddcda9d5674a495bda512fa267d62170211f69
Dragan Pepic <d.pepic@hotmail.com>
2020-04-22 20:04:11 +0200

    Add project files.

---
+++ KaoChat/Korisnik.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaoChat
+{
+	public class Korisnik : INotifyPropertyChanged
+	{
+		public string Ime { get; set; }
+
+		private Soba trenutnaSoba;
+		public Soba TrenutnaSoba 
+		{ 
+			get => trenutnaSoba; 
+			set
+			{
+				trenutnaSoba = value;
+				TrenutnaSoba.PosaljiPoruku += this.PrimiPoruku;
+			}
+		}
+
+		private string unos;
+		public string Unos 
+		{ 
+			get => unos;
+			set
+			{
+				unos = value;
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Unos"));
+			}
+		}
+
+		public List<string> Poruke { get; set; } = new List<string>();
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		//Prva stvar u lancu dogadjaja. Korisnik poziva metodu sobe i daje joj poruku
+		public void PoslajiPoruku()
+		{
+			TrenutnaSoba.PrimiPoruku(Unos);
+			Unos = "";
+		}
+
+		//Treci korak eventa, korisnik dobija poruku od eventa
+		public void PrimiPoruku(object soba, PosaljiPorukuArgs p)
+		{
+			Poruke.Add(p.Poruka);
+		}
+	}
+}
---
+++ KaoChat/Soba.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaoChat
+{
+
+	public class PosaljiPorukuArgs
+	{
+		public string Poruka;
+		public PosaljiPorukuArgs(string p)
+		{
+			Poruka = p;
+		}
+	}
+
+	public class Soba
+	{
+		public string Naziv { get; set; }
+
+		public delegate void PosaljiPorukuHandler(object soba, PosaljiPorukuArgs pp);
+		public event PosaljiPorukuHandler PosaljiPoruku;
+
+		//Drugi korak, soba prima poruku, i samo je prosledi eventu, koji poziva sve koji ga slusaju
+		public void PrimiPoruku(string p)
+		{
+			PosaljiPoruku?.Invoke(this, new PosaljiPorukuArgs(DateTime.Now + "---" + p));
+		}
+	}
+}
